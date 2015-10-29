a1a4bfd4fa2117e92db1041e9e90ed41ebeb8b62
Pascal Zingg <pzingg@fastforward.ch>
2015-10-29 15:08:34 +0100

    using numpy roll to shift (it's much easier than before :-))

--- src/prefixsum.py
+++ src/prefixsum.py
@@ -18,40 +18,28 @@ global c
 global dim
 
 
-def init_shift_left(matrix, index):
-    row_length = matrix.shape[0]
-    for l in range(index):
-        tmp = matrix[index][0]
-        for i in range(row_length - 1):
-            matrix[index][i] = matrix[index][i + 1]
-        matrix[index][-1] = tmp
+def init_shift_left(matrix):
+    size = matrix.shape[0]
+    for i in range(size):
+        matrix[i] = np.roll(matrix[i], i * -1, 0)
 
 
-def init_shift_up(matrix, index):
-    row_length = matrix.shape[0]
-    for l in range(index):
-        tmp = matrix[0][index]
-        for i in range(row_length - 1):
-            matrix[i][index] = matrix[i + 1][index]
-        matrix[-1][index] = tmp
+def init_shift_up(matrix):
+    tmatrix = np.transpose(matrix)
+    init_shift_left(tmatrix)
+    matrix = np.transpose(tmatrix)
 
 
 def shift_left(matrix):
-    dim = matrix.shape[0]
-    for i in range(dim):
-        tmp = matrix[i][0]
-        for j in range(dim - 1):
-            matrix[i][j] = matrix[i][j + 1]
-        matrix[i][-1] = tmp
+    size = matrix.shape[0]
+    for i in range(size):
+        matrix[i] = np.roll(matrix[i], -1, 0)
 
 
 def shift_up(matrix):
-    dim = matrix.shape[0]
-    for i in range(dim):
-        tmp = matrix[i][0]
-        for j in range(dim - 1):
-            matrix[i][j] = matrix[j + 1][i]
-        matrix[-1][i] = tmp
+    tmatrix = np.transpose(matrix)
+    shift_left(tmatrix)
+    matrix = np.transpose(tmatrix)
 
 
 def main():
@@ -67,13 +55,9 @@ def main():
         data = []
         # send the whole stuff to processors
         if rank == 0:
-            print("P%d (Core Proc) --------------------------" % rank)
             if turns == 0:
-                for i in range(dim):
-                    init_shift_left(a, i)
-
-                for i in range(dim):
-                    init_shift_up(b, i)
+                init_shift_left(a)
+                init_shift_up(b)
             else:
                 shift_left(a)
                 shift_up(b)
@@ -85,21 +69,18 @@ def main():
                     quarters_to_transfer = {'a': a_quarter, 'b': b_quarter}
                     data.append(quarters_to_transfer)
 
-            print("Sending: \n", data)
         data = comm.scatter(data, root=0)
 
         a_quarter = data.get('a')
         b_quarter = data.get('b')
-        steps = int(math.sqrt(dim))
         for i in range(steps):
             for j in range(steps):
                 result_quarter[i, j] = result_quarter[i, j] + a_quarter[i, j] + b_quarter[i, j]
-    
     data = comm.gather(result_quarter, root=0)
 
     # this is the last step
     if rank == 0:
-        matrix_to_add = np.zeros((1, 4))
+        matrix_to_add = np.zeros((1, dim))
         print(data)
         steps = int(math.sqrt(dim))
         for i in range(0, dim, steps):
@@ -109,9 +90,6 @@ def main():
         # Little hack to prevent dimension problem while concatenating
         matrix_to_add = np.delete(matrix_to_add, (0), axis=0)
         c = np.add(c, matrix_to_add)
-
-        shift_left(a)
-#        shift_up(b)
         print(c)
 
 
