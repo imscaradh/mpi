f37f2e2b03181a038ad874066eee9a938e86cbc0
Pascal Zingg <pzingg@fastforward.ch>
2015-10-29 17:44:23 +0100

    some improvments

--- src/prefixsum.py
+++ src/prefixsum.py
@@ -2,19 +2,6 @@ from mpi4py import MPI
 import numpy as np
 import math
 
-#a = np.array([
-#    [1, 2, 3, 4],
-#    [5, 6, 7, 8],
-#    [9, 10, 11, 12],
-#    [13, 14, 15, 16]])
-#
-#b = np.array([
-#    [1, 2, 3, 4],
-#    [5, 6, 7, 8],
-#    [9, 10, 11, 12],
-#    [13, 14, 15, 16]])
-#
-
 global a
 global b
 global c
@@ -49,10 +36,12 @@ def main():
     comm = MPI.COMM_WORLD
     dim = comm.Get_size()
     rank = comm.Get_rank()
+    wt = MPI.Wtime()
 
-    a = np.random.rand(dim, dim)
-    b = np.random.rand(dim, dim)
+    a = np.random.randint(1000, size=(dim, dim))
+    b = np.random.randint(1000, size=(dim, dim))
     c = np.zeros((dim, dim))
+
     steps = int(math.sqrt(dim))
     result_quarter = np.zeros((steps, steps))
 
@@ -61,6 +50,8 @@ def main():
         # send the whole stuff to processors
         if rank == 0:
             if turns == 0:
+                print("A matrix: \n", a)
+                print("\nB matrix:\n", b)
                 init_shift_left(a)
                 init_shift_up(b)
             else:
@@ -84,18 +75,19 @@ def main():
 
     # this is the last step
     if rank == 0:
-        matrix_to_add = np.zeros((1, dim))
-        print(data)
+        matrix_to_add = np.zeros((0, dim))
         steps = int(math.sqrt(dim))
         for i in range(0, dim, steps):
-            temp = np.concatenate((data[i], data[i + 1]), axis=1)
+            temp = np.zeros((steps, 0))
+            for j in range(0, dim, steps):
+                temp = np.concatenate((temp, data[j]), axis=1)
             matrix_to_add = np.concatenate((matrix_to_add, temp), axis=0)
 
-        # Little hack to prevent dimension problem while concatenating
-        matrix_to_add = np.delete(matrix_to_add, (0), axis=0)
         c = np.add(c, matrix_to_add)
-        print(c)
+        print("\n Result:\n", repr(c))
 
+        used_time = MPI.Wtime() - wt
+        print("Used time: %d ms" % (used_time * 1000))
 
 if __name__ == '__main__':
     main()
