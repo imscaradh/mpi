4467fe7d0a2e0be121c3442f02a4d2a2dfc9fbcd
Pascal Zingg <pzingg@fastforward.ch>
2015-11-05 16:23:31 +0100

    tried to improve running time, not completed yet

--- src/matrixmul.py
+++ src/matrixmul.py
@@ -17,43 +17,39 @@ def init_shift_left(matrix):
 def init_shift_up(matrix):
     tmatrix = np.transpose(matrix)
     init_shift_left(tmatrix)
-    matrix = np.transpose(tmatrix)
+    np.transpose(tmatrix)
 
 
 def shift_left(matrix):
-    size = matrix.shape[0]
-    for i in range(size):
-        matrix[i] = np.roll(matrix[i], -1, 0)
+    np.roll(matrix, -1, 0)
 
 
 def shift_up(matrix):
-    tmatrix = np.transpose(matrix)
-    shift_left(tmatrix)
-    matrix = np.transpose(tmatrix)
+    shift_left(np.transpose(matrix))
 
 
 def main():
     comm = MPI.COMM_WORLD
     procs = comm.Get_size()
     rank = comm.Get_rank()
-    wt = MPI.Wtime()
 
-    N = 16
+    N = 3000
 
-    a = np.random.randint(1000, size=(N, N))
-    b = np.random.randint(1000, size=(N, N))
+    a = np.random.randint(400, size=(N, N))
+    b = np.random.randint(400, size=(N, N))
     c = np.zeros((N, N))
 
     steps = int(N / math.sqrt(procs))
-    result_quarter = np.zeros((steps, steps))
+    result_quarter = np.empty((steps, steps))
+
+    sendData = np.empty([2 * steps, steps], dtype='i')
+    recvData = np.empty([2 * steps, steps], dtype='i')
 
     for turns in range(N):
-        data = []
+        wt = MPI.Wtime()
         # send the whole stuff to processors
         if rank == 0:
             if turns == 0:
-                print("A matrix: \n", repr(a))
-                print("\nB matrix:\n", repr(b))
                 init_shift_left(a)
                 init_shift_up(b)
             else:
@@ -64,31 +60,32 @@ def main():
                 for j in range(0, N, steps):
                     a_quarter = a[i:i + steps, j:j + steps]
                     b_quarter = b[i:i + steps, j:j + steps]
-                    quarters_to_transfer = {'a': a_quarter, 'b': b_quarter}
-                    data.append(quarters_to_transfer)
-        data = comm.scatter(data, root=0)
+                    sendData[0:steps, 0:steps] = a_quarter
+                    sendData[steps:, 0:steps] = b_quarter
 
-        a_quarter = data.get('a')
-        b_quarter = data.get('b')
-        for i in range(steps):
-            for j in range(steps):
-                result_quarter[i, j] = result_quarter[i, j] + a_quarter[i, j] * b_quarter[i, j]
-    data = comm.gather(result_quarter, root=0)
+        comm.Scatter(sendData, recvData, root=0)
+
+        a_quarter = recvData[0:steps]
+        b_quarter = recvData[steps:]
+        result_quarter = np.multiply(a_quarter, b_quarter)
+
+        used_time = MPI.Wtime() - wt
+        print("Proc", rank, ", time: ", (used_time * 1000))
 
     # this is the last step
+    comm.Gather(result_quarter, recvData, root=0)
     if rank == 0:
         quarter_width = int(math.sqrt(procs))
         c = np.zeros((0, N))
         for i in range(quarter_width):
             temp = np.zeros((steps, 0))
             for j in range(i * quarter_width, (i + 1) * quarter_width):
-                temp = np.concatenate((temp, data[j]), axis=1)
+                temp = np.concatenate((temp, recvData[j]), axis=1)
             c = np.concatenate((c, temp), axis=0)
 
-        print("\n Result:\n", repr(c))
-
         used_time = MPI.Wtime() - wt
-        print("Used time: %d ms" % (used_time * 1000))
+        print("Used time: ", used_time * 1000)
+
 
 if __name__ == '__main__':
     main()
